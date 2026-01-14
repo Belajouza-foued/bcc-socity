14a330d91f7809e05df9d2f0867088852a410db0
Belajouza-foued <157969302+Belajouza-foued@users.noreply.github.com>
2026-01-14 18:04:41 +0100

    mission accompli pour portfolio

---
+++ front-end/src/components/CardDate.js
@@ -0,0 +1,83 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+function CardDATE() {
+    return (
+      <>
+         <div className='card-position1 text-center mx-auto ps-5 '>
+    <div className='row text-center mt-5 pt-5'>
+      <div className="col-lg-4 col-md-6 mb-4">
+      <div className="card card-top" style={{ width:"360px", height:"370px", borderRadius:"15px 15px"}}>
+        <div className='card-img-top1'></div>
+        
+        <div className="card-body text-center">
+          <div className='row'>
+            <div className='col-lg-3 col-sm-12' style={{lineHeight:"0.9"}}>
+          <span className="pt-2 ps-1"style={{color:'#c32942', fontWeight:"700",fontSize:"13px"}}>Janvier</span>
+          <p style={{color:"#5b99d3",fontSize:"40px",fontWeight:"700"}} className="">19</p>
+          </div>
+     <div className='col-lg-9 col-sm-12 pt-3'>
+ <div className="card-text"style={{lineHeight:"0.5",position:"relative",left:'-25px'}}>
+   <h5 className='' style={{fontWeight:"600",fontSize:"18px"}}>Design graphique</h5>
+   <p  className='ps-3' style={{fontWeight:"400",fontSize:"16px",letterSpacing:"1px"}}>16 au 19 janvier 2025</p>
+   <span className='' style={{fontWeight:"400",fontSize:"13px",color:"#6A6A6A"}}>PARNASSE CIBLES 1 À 32</span>
+          </div>
+          </div>
+                   </div>
+        </div>
+      </div>
+    </div>
+     <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+      <div className="card card-top" style={{ width:"360px", height:"370px", borderRadius:"15px 15px"}}>
+       <div className='card-img-top1'></div>
+        <div className="card-body text-center">
+          <div className='row'>
+            <div className='col-lg-3 col-sm-12' style={{lineHeight:"0.9"}}>
+          <span className="pt-2 ps-1"style={{color:'#c32942', fontWeight:"700",fontSize:"13px"}}>Janvier</span>
+          <p style={{color:"#5b99d3",fontSize:"40px",fontWeight:"700"}} className="">20</p>
+          </div>
+    <div className='col-lg-9 col-sm-12 pt-2'>
+ <div className="card-text"style={{lineHeight:"0.6",position:"relative",left:'-30px'}}>
+   <h5 className='' style={{fontWeight:"600",fontSize:"18px"}}>Design graphique</h5>
+   <p className='ps-3' style={{fontWeight:"400",fontSize:"16px", letterSpacing:"1px"}}>16 au 19 janvier 2025</p>
+   <span className='' style={{fontWeight:"400",fontSize:"13px",color:"#6A6A6A"}}>PARNASSE CIBLES 1 À 32</span>
+          </div>
+          </div>
+                   </div>
+        </div>
+      </div>
+    </div>
+      <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+      <div className="card card-top" style={{ width:"360px", height:"370px", borderRadius:"15px 15px"}}>
+       <div className='card-img-top1'></div>
+        <div className="card-body text-center">
+          <div className='row'>
+            <div className='col-lg-3 col-sm-12' style={{lineHeight:"0.9"}}>
+          <span className="pt-2 ps-1"style={{color:'#c32942', fontWeight:"700",fontSize:"13px"}}>Janvier</span>
+          <p style={{color:"#5b99d3",fontSize:"40px",fontWeight:"700"}} className="">20</p>
+          </div>
+    <div className='col-lg-9 col-sm-12 pt-2'>
+ <div className="card-text"style={{lineHeight:"0.6",position:"relative",left:'-30px'}}>
+   <h5 className='' style={{fontWeight:"600",fontSize:"18px"}}>Design graphique</h5>
+   <p className='ps-3' style={{fontWeight:"400",fontSize:"16px", letterSpacing:"1px"}}>16 au 19 janvier 2025</p>
+   <span className='' style={{fontWeight:"400",fontSize:"13px",color:"#6A6A6A"}}>PARNASSE CIBLES 1 À 32</span>
+          </div>
+          </div>
+                   </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    
+   
+  
+ 
+ </div>
+</div>
+    
+  
+     </>
+    );
+  }
+  export default CardDATE;
+  
--- front-end/src/pages/MarkEvenment.js
+++ front-end/src/pages/MarkEvenment.js
@@ -9,6 +9,7 @@ import imgLocal from './images/local.png'
 import imgMap from './images/image-map.png'
 import imgBaffle from './images/img-baffle.png'
 import logoMap from "./images/logo-map.png"
+import CardDATE from '../components/CardDate';
 const MarkEvenment  = () => {
     return(  <>
 
@@ -128,74 +129,8 @@ nous vous invitons a assisster à nos formations, participer à des workshops, �
             <h1 style={{color:"#393738", fontWeight:"700",fontSize:"40px", fontFamily:"Poppins"}}>Autres Evènements Qui Pourraient Vous</h1>
             <h1 style={{color:"#393738", fontWeight:"700",fontSize:"40px", fontFamily:"Poppins"}}>Plaire</h1>
         </div>
-        <div className='card-position1 text-center mx-auto'>
-    <div className='row text-center mt-5 pt-5'>
-      <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card card-top" style={{ width:"360px", height:"370px", borderRadius:"15px 15px"}}>
-        <div className='card-img-top1'></div>
-        
-        <div className="card-body text-center">
-          <div className='row'>
-            <div className='col-lg-3 col-sm-12' style={{lineHeight:"0.9"}}>
-          <span className="pt-2 ps-1"style={{color:'#c32942', fontWeight:"700",fontSize:"15px"}}>Janvier</span>
-          <p style={{color:"#5b99d3",fontSize:"40px",fontWeight:"700"}} className="">19</p>
-          </div>
-     <div className='col-lg-9 col-sm-12'>
- <p className="card-text"style={{lineHeight:"0.8",position:"relative",left:'-30px'}}>
-   <h5 className='pe-5 me-3' style={{fontWeight:"600",fontSize:"17px"}}>Design graphique</h5>
-   <p  style={{fontWeight:"400",fontSize:"17px"}}>2025 16 au 19 janvier 2025</p>
-   <span className='pe-4 me-1' style={{fontWeight:"400",fontSize:"15.20px",color:"#6A6A6A"}}>PARNASSE CIBLES 1 À 32</span>
-          </p>
-          </div>
-                   </div>
-        </div>
-      </div>
-    </div>
-     <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
-      <div className="card card-top" style={{ width:"360px", height:"370px", borderRadius:"15px 15px"}}>
-       <div className='card-img-top1'></div>
-        <div className="card-body text-center">
-          <div className='row'>
-            <div className='col-lg-3 col-sm-12' style={{lineHeight:"0.9"}}>
-          <span className="pt-2 ps-1"style={{color:'#c32942', fontWeight:"700",fontSize:"15px"}}>Janvier</span>
-          <p style={{color:"#5b99d3",fontSize:"40px",fontWeight:"700"}} className="">20</p>
-          </div>
-    <div className='col-lg-9 col-sm-12'>
- <p className="card-text"style={{lineHeight:"0.8",position:"relative",left:'-30px'}}>
-   <h5 className='pe-5 me-3' style={{fontWeight:"600",fontSize:"17px"}}>Design graphique</h5>
-   <p  style={{fontWeight:"400",fontSize:"17px"}}>2025 16 au 19 janvier 2025</p>
-   <span className='pe-4 me-1' style={{fontWeight:"400",fontSize:"15.20px",color:"#6A6A6A"}}>PARNASSE CIBLES 1 À 32</span>
-          </p>
-          </div>
-                   </div>
-        </div>
-      </div>
-    </div>
-     <div className="col-lg-4 col-md-6 mb-4 col-sm-12">
-      <div className="card card-top" style={{ width:"360px", height:"370px", borderRadius:"15px 15px"}}>
-                <div className='card-img-top1'></div>
-        <div className="card-body text-center">
-          <div className='row'>
-            <div className='col-3' style={{lineHeight:"0.9"}}>
-          <span className="pt-2 ps-1"style={{color:'#c32942', fontWeight:"700",fontSize:"15px"}}>Janvier</span>
-          <p style={{color:"#5b99d3",fontSize:"40px",fontWeight:"700"}} className="">21</p>
-          </div>
-     <div className='col-lg-9 col-sm-12'>
- <p className="card-text"style={{lineHeight:"0.8",position:"relative",left:'-30px'}}>
-   <h5 className='pe-5 me-3' style={{fontWeight:"600",fontSize:"17px"}}>Design graphique</h5>
-   <p  style={{fontWeight:"400",fontSize:"17px"}}>2025 16 au 19 janvier 2025</p>
-   <span className='pe-4 me-1' style={{fontWeight:"400",fontSize:"15.20px",color:"#6A6A6A"}}>PARNASSE CIBLES 1 À 32</span>
-          </p>
-          </div>
-                   </div>
-        </div>
-      </div>
-    </div>
-    
- 
- </div>
-</div>
- <div className='row pt-5 mt-5 position-relative'>
+     <CardDATE/>
+ <div className='row pt-5 mt-5 position-relative baffle-res'>
  <div className='col-2 ps-3'>
 <div className='image-baffle'>
     <img src={imgBaffle} alt='' className='img-baffle'/>
